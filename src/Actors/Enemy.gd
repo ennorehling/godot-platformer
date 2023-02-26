81ce24136229be2928a7efe43a63c11287ed1baa
Enno Rehling <enno.rehling@gmail.com>
2023-02-26 19:50:27 +0100

    initial commit
    enemies are moving

---
+++ src/Actors/Enemy.gd
@@ -0,0 +1,12 @@
+extends Actor
+class_name Enemy
+
+func _ready() -> void:
+	set_physics_process(false)
+	_velocity.x = - speed.x
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
